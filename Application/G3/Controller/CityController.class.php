d0a0dad8c2b68646e6ae0e8c1d224967c1cfa6a4
luchao <lc_php@126.com>
2017-05-15 13:08:33 +0800

    城市切换

--- Application/G3/Controller/CityController.class.php
+++ Application/G3/Controller/CityController.class.php
@@ -12,6 +12,15 @@ use Think\Controller;
  * 切换城市
  */
 class CityController extends Controller {
+    protected function _initialize(){
+        /* 读取站点配置 */
+        $config = api('Config/lists');
+        C($config); //添加配置
+
+        if(!C('WEB_SITE_CLOSE')){
+            $this->error('站点已经关闭，请稍后访问~');
+        }
+    }
 
 	public function index(){
         $pca_p = getNextPca(0);
